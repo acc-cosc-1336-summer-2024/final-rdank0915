f4e53dc45e05ae184fe309bcf66210abc131395d
rdank0915 <rdank0915@gmail.com>
2024-07-23 19:04:44 +0000

    Final - question_c

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,1 +1,17 @@
 #add import
+
+import question_c
+
+class Portfolio:
+
+    def __init__(self, name):
+        self.__name = name
+        self.__stocks = []
+    
+    def add_stock(self, stock):
+        self.__stocks.append(stock)
+    
+    def display_portfolio(self):
+        print(f"Stock Portfolio: {self.__name}")
+        for stock in self.__stocks:
+            print(f"{stock.get_symbol()} - {stock.get_company_name()}")
--- src/question_c/question_c.py
+++ src/question_c/question_c.py
@@ -1,1 +1,28 @@
 #write functions here, don't add input('') statements here!
+
+class Stock:
+    
+    #Create a constructor that accepts symbol and company_name
+    def __init__(self, symbol, company_name):
+        self.__symbol = symbol
+        self.__company_name = company_name
+    
+    #Create a stock class function/method that returns symbol
+    def get_symbol(self):
+        return self.__symbol
+    
+    #Create a stock class function/method that returns company_name
+    def get_company_name(self):
+        return self.__company_name
+        
+#Create a function stock_list with 5 variables/instances of Stock
+#with the values from the Stock List
+def stock_list():
+
+    stock1 = Stock("AAPL", "Apple Inc")
+    stock2 = Stock("CAT", "Caterpillar")
+    stock3 = Stock("EK", "Eastman Kodak")
+    stock4 = Stock("GOOG", "Google")
+    stock5 = Stock("MSFT", "Microsoft")
+
+   
