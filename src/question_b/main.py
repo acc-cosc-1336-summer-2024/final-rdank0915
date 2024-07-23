65b42787678779151daacbd140ba708d4fd47fd7
rdank0915 <rdank0915@gmail.com>
2024-07-23 05:15:22 +0000

    Final - question_b

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,1 +1,9 @@
 #add import
+
+import question_b
+
+rows = int(input("How many rows in your matrix?: "))
+cols = int(input("How many columns in your matrix?: "))
+
+list = question_b.create_multiplication_table(rows, cols)
+table = question_b.display_multiplication_table(list)
--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,19 @@
 #write functions here, don't add input('') statements here!
+
+#Create a create_multiplication_table function that returns a list
+def create_multiplication_table(rows, cols):
+
+    list = []
+
+    for i in range(0, rows):
+        row_list = []
+        for j in range(0, cols):
+            row_list.append((i+1) * (j+1))
+        list.append(row_list)
+
+    return(list)
+
+#Create a display_multiplication_table that displays a table and accepts a list parameter
+def display_multiplication_table(list):
+    for row in list:
+        print(row)
