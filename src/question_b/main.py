f70bbaf4aade224dfd6d865b4e4c964d014c0276
rdank0915 <rdank0915@gmail.com>
2024-07-23 06:07:39 +0000

    Final - question_b

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -2,8 +2,4 @@
 
 import question_b
 
-rows = int(input("How many rows in your matrix?: "))
-cols = int(input("How many columns in your matrix?: "))
-
-list = question_b.create_multiplication_table(rows, cols)
-table = question_b.display_multiplication_table(list)
+question_b.run_menu()
--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -17,3 +17,43 @@ def create_multiplication_table(rows, cols):
 def display_multiplication_table(list):
     for row in list:
         print(row)
+
+def display_menu():
+    print("Question B Menu")
+    print("1-Create a multiplication matrix")
+    print("2-Exit")
+
+def run_menu():
+    option = "1"
+    while(option != "2"):
+        display_menu()
+        option = input("Enter your menu option: ")
+        handle_menu_option(option)
+
+def handle_menu_option(option):
+    if(option == '1'):
+        option_1()
+    elif(option == '2'):
+        option_2()
+    else:
+        print("Invalid Option")
+
+def option_1():
+
+    rows = int(input("How many rows in your matrix?: "))
+    cols = int(input("How many columns in your matrix?: "))
+    list = create_multiplication_table(rows, cols)
+    table = display_multiplication_table(list)
+    print(table)
+    
+
+    choice = "Y"
+    while(choice == "Y" or choice == "N"):
+        choice = input("Would you like to exit Y or N: ")
+        if choice == "Y":
+            print(run_menu())
+        elif choice == "N":
+            print(option_1())
+
+def option_2():
+    print("Exiting the program...")
